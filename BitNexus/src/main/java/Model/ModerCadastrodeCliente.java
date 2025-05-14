c97509f1d8d152c824f5e7f2e2010996c61a791b
ALUNO <ALUNO@EAI>
2025-05-14 17:34:52 -0300

    A criação de Caixa

---
+++ BitNexus/src/main/java/Model/ModerCadastrodeCliente.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author ALUNO
+ */
+public class ModerCadastrodeCliente {
+    
+    String nomeDoCliente;
+    String seleciaServico;
+    String nomeMaquina;
+    String pagamemtoDoServico;
+    String formaDePagar;
+    String ligarMarquinar;
+
+    public ModerCadastrodeCliente(String nomeDoCliente, String seleciaServico, String nomeMaquina, String pagamemtoDoServico, String formaDePagar) {
+        this.nomeDoCliente = nomeDoCliente;
+        this.seleciaServico = seleciaServico;
+        this.nomeMaquina = nomeMaquina;
+        this.pagamemtoDoServico = pagamemtoDoServico;
+        this.formaDePagar = formaDePagar;
+    }
+    
+    
+    
+}
