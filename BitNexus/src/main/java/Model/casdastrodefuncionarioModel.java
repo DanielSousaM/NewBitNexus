d39cc2d1c38be0ff12028dcda845dc64c20e555b
ALUNO <emanoelregomaques05@gmail.com>
2025-05-14 17:53:06 -0300

    criaçao clase cadastrado funcionario

---
+++ BitNexus/src/main/java/Model/casdastrodefuncionarioModel.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author ALUNO
+ */
+public class casdastrodefuncionarioModel {
+   //cadeia -> string
+   //interio-> int
+   // real -> doublo ou float
+   // logico -> boolean
+   // datas -> Date
+   
+    String nomecompleto;
+    String datadenacimento;
+    String cpf;
+    String fone;
+    String email;
+    String senha;
+
+    public casdastrodefuncionarioModel(String nomecompleto, String datadenacimento, String cpf, String fone, String email, String senha) {
+        this.nomecompleto = nomecompleto;
+        this.datadenacimento = datadenacimento;
+        this.cpf = cpf;
+        this.fone = fone;
+        this.email = email;
+        this.senha = senha;
+    }
+    
+    
+
+    
+   
+   
+    
+     
+}
