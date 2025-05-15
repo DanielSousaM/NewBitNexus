6f1efa8a6c4e26a9000eb28f4134595c408c616d
ALUNO <ALUNO@EAI>
2025-05-15 15:11:01 -0300

    A criação de código Financeiro

--- BitNexus/src/main/java/Model/ModerCadastrodeCliente.java
+++ BitNexus/src/main/java/Model/ModerCadastrodeCliente.java
@@ -10,12 +10,12 @@ package Model;
  */
 public class ModerCadastrodeCliente {
     
-    String nomeDoCliente;
-    String seleciaServico;
-    String nomeMaquina;
-    String pagamemtoDoServico;
-    String formaDePagar;
-    String ligarMarquinar;
+    private String nomeDoCliente;
+    private String seleciaServico;
+    private String nomeMaquina;
+    private String pagamemtoDoServico;
+    private String formaDePagar;
+    private String ligarMarquinar;
 
     public ModerCadastrodeCliente(String nomeDoCliente, String seleciaServico, String nomeMaquina, String pagamemtoDoServico, String formaDePagar) {
         this.nomeDoCliente = nomeDoCliente;
@@ -24,6 +24,60 @@ public class ModerCadastrodeCliente {
         this.pagamemtoDoServico = pagamemtoDoServico;
         this.formaDePagar = formaDePagar;
     }
+     
+     // metodos getter e setter
+
+    public String getNomeDoCliente() {
+        return nomeDoCliente;
+    }
+
+    public void setNomeDoCliente(String nomeDoCliente) {
+        this.nomeDoCliente = nomeDoCliente;
+    }
+    // metodo com retorno do tipo String
+    public String getSeleciaServico() {
+        return seleciaServico;
+    }
+    // metodo com parametro e sem retorno
+    public void setSeleciaServico(String seleciaServico) {
+        this.seleciaServico = seleciaServico;
+    }
+
+    public String getNomeMaquina() {
+        return nomeMaquina;
+    }
+
+    public void setNomeMaquina(String nomeMaquina) {
+        this.nomeMaquina = nomeMaquina;
+    }
+
+    public String getPagamemtoDoServico() {
+        return pagamemtoDoServico;
+    }
+
+    public void setPagamemtoDoServico(String pagamemtoDoServico) {
+        this.pagamemtoDoServico = pagamemtoDoServico;
+    }
+
+    public String getFormaDePagar() {
+        return formaDePagar;
+    }
+
+    public void setFormaDePagar(String formaDePagar) {
+        this.formaDePagar = formaDePagar;
+    }
+
+    public String getLigarMarquinar() {
+        return ligarMarquinar;
+    }
+
+    public void setLigarMarquinar(String ligarMarquinar) {
+        this.ligarMarquinar = ligarMarquinar;
+    }
+    
+    
+    
+    
     
     
     
