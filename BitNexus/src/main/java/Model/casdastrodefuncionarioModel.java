f1dc2070442d98b53b87b7aad20432b70ba8114a
ALUNO <emanoelregomaques05@gmail.com>
2025-05-15 15:11:07 -0300

    criaçao de código funcionário

--- BitNexus/src/main/java/Model/casdastrodefuncionarioModel.java
+++ BitNexus/src/main/java/Model/casdastrodefuncionarioModel.java
@@ -15,12 +15,12 @@ public class casdastrodefuncionarioModel {
    // logico -> boolean
    // datas -> Date
    
-    String nomecompleto;
-    String datadenacimento;
-    String cpf;
-    String fone;
-    String email;
-    String senha;
+    private String nomecompleto;
+    private String datadenacimento;
+    private String cpf;
+    private String fone;
+    private String email;
+    private String senha;
 
     public casdastrodefuncionarioModel(String nomecompleto, String datadenacimento, String cpf, String fone, String email, String senha) {
         this.nomecompleto = nomecompleto;
@@ -31,11 +31,61 @@ public class casdastrodefuncionarioModel {
         this.senha = senha;
     }
     
-    
+       // metados getter e setter
+
+       //gertter serve para visualizar os dados
+    public String getNomecompleto() {
+        return nomecompleto;
+    }
+       // setter serve para guarda dados
+    public void setNomecompleto(String nomecompleto) {
+        this.nomecompleto = nomecompleto;
+    }
+
+    public String getDatadenacimento() {
+        return datadenacimento;
+    }
+
+    public void setDatadenacimento(String datadenacimento) {
+        this.datadenacimento = datadenacimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+       
 
     
    
    
     
      
-}
+} // chve da class
